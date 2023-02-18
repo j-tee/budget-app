d46e4c122ed2b7a9d5ad0028c13a6347f8c290ca
j-tee <juliustetteh@gmail.com>
2023-02-18 17:21:32 +0000

    Implement required changes

--- spec/factories/transaction_items.rb
+++ spec/factories/transaction_items.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :transaction_item do
     name { Faker::Lorem.word }
     amount { Faker::Number.decimal(l_digits: 2) }
-    user
+    association :author, factory: :user
   end
 end
